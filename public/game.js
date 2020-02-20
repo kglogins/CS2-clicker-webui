142b99700fe681e78237d56e48582dbe0ef9f94f
Karlis Gustavs Logins <karlis.gustavs.logins@gmail.com>
2020-02-20 20:16:06 +0200

    Version 1.0

--- public/game.js
+++ public/game.js
@@ -1,20 +1,31 @@
 
 // Change "test" to any username you'd like to start a new game
-var username = "test";
+var username = "kglogins";
 
 var socket = io.connect("https://tictactoe.info/", {transports: ['websocket']});
 setupSocket();
 
 
 function setupSocket() {
+    var name = document.getElementById('name');
+    var currentGold = document.getElementById('currentGold');
+    var shovel = document.getElementById('shovel');
+    var excavator = document.getElementById('excavator');
+    var mine = document.getElementById('mine');
+
 
     // This function is called whenever a new game state is received from the server
     socket.on('gameState', function (jsonGameState) {
-        console.log(jsonGameState);
+        var data = JSON.parse(jsonGameState);
 
         // TODO: Display the game state on your GUI
         // You must display: current gold, and the name, number owned, and cost for each type of equipment
 
+        name.innerHTML = data.username;
+        currentGold.innerHTML = data.gold;
+        shovel.innerHTML = `${data.equipment.shovel.name} | Buy for ${data.equipment.shovel.cost} | Owned: ${data.equipment.shovel.numberOwned}`;
+        excavator.innerHTML = `${data.equipment.excavator.name} | Buy for ${data.equipment.excavator.cost} | Owned: ${data.equipment.excavator.numberOwned}`;
+        mine.innerHTML = `${data.equipment.mine.name} | Buy for ${data.equipment.mine.cost} | Owned: ${data.equipment.mine.numberOwned}`;
     });
 }
 
